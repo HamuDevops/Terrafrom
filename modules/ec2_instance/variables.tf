d826e86fed3bcfd2c59ab4fa58e3e19dfcf7ece8
HamuDevops <hnarwade12@gmail.com>
2025-09-16 16:31:32 +0000

    .

---
+++ modules/ec2_instance/variables.tf
@@ -0,0 +1,13 @@
+    variable "ami" {
+        description = "value of ami"
+        type = string
+        
+    }
+    variable "instance_type" {
+        description = "value of instance_type"
+        type = string
+    }
+    variable "subnet_id" {
+        description = "value of subnet_id"
+        type = string
+    }
